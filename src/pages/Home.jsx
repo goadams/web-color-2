3cd5cf52431c2585a94c7e29d31ced84b7116e95
aGregAdams <gregory.adams29@gmail.com>
2025-07-16 18:40:36 -0400

    Remove header from home page Layout component, add branding to the top of home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,16 @@ import Layout from "../components/Layout.jsx";
 const Home = () => {
     return (
         <>
-            <Layout>
+            <Layout header={false}>
+                <Link to="/" className="logo-link logo-link-home" aria-label="Web Color Tools Home">
+                    <svg width="64" height="64" viewBox="0 0 64 64" aria-labelledby="wct-logo-title" role="img" xmlns="http://www.w3.org/2000/svg">
+                        <title id="wct-logo-title">Web Color Tools Minimalist Logo</title>
+                        <circle cx="24" cy="32" r="15" fill="#FF8DA1"/>
+                        <circle cx="32" cy="54" r="15" fill="#9C9CEA"/>
+                        <circle cx="40" cy="10" r="15" fill="#FFD966"/>
+                    </svg>
+                    Web Color Tools
+                </Link>
                 <h1>Home Page</h1>
             </Layout>
         </>
