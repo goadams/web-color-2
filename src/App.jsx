1af125a704f239616c2f266eee1f7920a1793ac8
aGregAdams <gregory.adams29@gmail.com>
2025-07-16 15:45:20 -0400

    Implement react-router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Accessibility from './pages/Accessibility';
+import ColorPalettes from './pages/ColorPalettes';
+import Gradients from './pages/Gradients';
+import RandomColor from './pages/RandomColor';
+import Home from './pages/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/random" element={<RandomColor />} />
+                <Route path="/palettes" element={<ColorPalettes />} />
+                <Route path="/gradients" element={<Gradients />} />
+                <Route path="/accessibility" element={<Accessibility />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
