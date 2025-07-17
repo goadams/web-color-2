a04c11332c28c07acb0034eb595f8ef1ff3eea58
aGregAdams <gregory.adams29@gmail.com>
2025-07-17 17:05:53 -0400

    Update best text color options to #000000 and #FFFFFF

--- src/utils/getTextBestColor.js
+++ src/utils/getTextBestColor.js
@@ -1,7 +1,7 @@
 import getContrastRatio from "./getContrastRatio.js";
 
 export default function getTextBestColor(backgroundColor) {
-    const whiteContrast = getContrastRatio(backgroundColor, '#DDDDFF');
-    const blackContrast = getContrastRatio(backgroundColor, '#111111');
-    return whiteContrast > blackContrast ? '#DDDDFF' : '#111111';
+    const whiteContrast = getContrastRatio(backgroundColor, '#FFFFFF');
+    const blackContrast = getContrastRatio(backgroundColor, '#000000');
+    return whiteContrast > blackContrast ? '#FFFFFF' : '#000000';
 }
