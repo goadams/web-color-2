767d46f5a47dff0d5059eac07f12245a61cb10c8
aGregAdams <gregory.adams29@gmail.com>
2025-07-17 18:42:33 -0400

    Rework SavedSection component to work and style well for ColorPalettes.jsx and RandomColor.jsx

--- src/components/SavedSection.jsx
+++ src/components/SavedSection.jsx
@@ -5,13 +5,15 @@ import "./SavedSection.css";
 const SavedSection = ({ colors = [], handleDelete, title = '' }) => (
     <div className="saved-color-wrapper">
         {title && <p className="saved-title">{title}</p>}
-        {colors.map((c, i) => (
-            <div className="saved-display" key={i} >
-                <p className="saved-color-hex">{c}</p>
-                <p className="saved-color-rgb">rgb({hexToRgb(c).r}, {hexToRgb(c).g}, {hexToRgb(c).b})</p>
-                <div className="saved-color" style={{ background: c, width: "10rem", height: "5rem" }}></div>
-            </div>
-        ))}
+        <div className="saved-color-set">
+            {colors.map((c, i) => (
+                <div className="saved-display" key={i} >
+                    <p className="saved-color-hex">{c}</p>
+                    <p className="saved-color-rgb">rgb({hexToRgb(c).r}, {hexToRgb(c).g}, {hexToRgb(c).b})</p>
+                    <div className="saved-color" style={{ background: c, width: "10rem", height: "5rem" }}></div>
+                </div>
+            ))}
+        </div>
         <button className="saved-delete" onClick={handleDelete}>Delete</button>
     </div>
 );
