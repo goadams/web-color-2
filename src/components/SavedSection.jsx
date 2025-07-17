6d5bddde937250b97038c5245d2d878bb37734ac
aGregAdams <gregory.adams29@gmail.com>
2025-07-17 16:58:54 -0400

    Create SavedSection.jsx component for use in multiple pages

---
+++ src/components/SavedSection.jsx
@@ -0,0 +1,19 @@
+import hexToRgb from "../utils/hexToRbg.js";
+import React from "react";
+import "./SavedSection.css";
+
+const SavedSection = ({ colors = [], handleDelete, title = '' }) => (
+    <div className="saved-color-wrapper">
+        {title && <p className="saved-title">{title}</p>}
+        {colors.map((c, i) => (
+            <div className="saved-display" key={i} >
+                <p className="saved-color-hex">{c}</p>
+                <p className="saved-color-rgb">rgb({hexToRgb(c).r}, {hexToRgb(c).g}, {hexToRgb(c).b})</p>
+                <div className="saved-color" style={{ background: c, width: "10rem", height: "5rem" }}></div>
+            </div>
+        ))}
+        <button className="saved-delete" onClick={handleDelete}>Delete</button>
+    </div>
+);
+
+export default SavedSection;
