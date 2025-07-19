1dbad5f38e9a96598f150883d32e691a027b7917
aGregAdams <gregory.adams29@gmail.com>
2025-07-19 16:52:24 -0400

    Create reusable OptionSelect component

---
+++ src/components/OptionSelect.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Select, { components as Components } from 'react-select';
+import "./OptionSelect.css"
+
+const MaxLengthInput = (props) => {
+    const { maxLength } = props.selectProps;
+    return <Components.Input {...props} maxLength={maxLength} />;
+};
+
+const OptionSelect = ({ children, name, value, styles, options, onChange, maxLength = 25 }) => {
+    const modName = name.replace(/\s+/g, '-').toLowerCase();
+
+    return (
+        <div className="os-container">
+            <div className="os-select-wrapper">
+                <label htmlFor={`${modName}-select`}>{name} Select:</label>
+                <Select
+                    className={`${modName}-select os-select`}
+                    components={{Input: MaxLengthInput}}
+                    maxLength={maxLength}
+                    inputId={`${modName}-select`}
+                    name={`${modName}-select`}
+                    value={value}
+                    styles={styles}
+                    options={options}
+                    onChange={onChange}
+                />
+            </div>
+            {children}
+        </div>
+)};
+
+export default OptionSelect;
